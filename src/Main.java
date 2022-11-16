3d934ea117120d6089f9353570b9a7f1d2ccc01d
mishe <mishel1400@mail.ru>
2022-11-16 11:00:25 +0300

    Сделала ДЗ

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Human Maxim = new Human("Максим", 1988, "Минск","бренд-менеджер");
+        Human Anna = new Human("Аня", 1993, "Москва", "методист образовательных программ");
+        Human Kate = new Human("Катя",1992, "Калининград","продакт-менеджер");
+        Human Artem = new Human("Артем",1995,"Москва","директор по развитию бизнеса");
+    }
+}
