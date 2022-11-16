addcfd4dca513a666cc6d0f0e16f8e0bddf896fc
mishe <mishel1400@mail.ru>
2022-11-16 11:04:06 +0300

    Сделала ДЗ

--- src/Human.java
+++ src/Human.java
@@ -11,4 +11,5 @@ public class Human {
         this.jobTitle = jobTitle;
         System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности "+ jobTitle +". Будем знакомы!");
     }
+
 }
